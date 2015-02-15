0ae45b4f00e1babbe1af2c9b0a4f5b919da2572b
Doychin Atanasov <ironsmile@gmail.com>
2015-02-15 12:16:05 +0200

    Initial commit with theremin-like device

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arduino Projects
+
+This is a repo in which I will store my arduino projects. Every directry is a single project. Probably all the instructions in the project files are in Bulgarian.
---
+++ theremin-like/theremin.ino
@@ -0,0 +1,67 @@
+#define DEBUG (0)
+
+//  ?????? ?? ???? ? ?????
+const int ldr_pin = A0;  // ??????? ?? ??? ??? ? ??????? ???????
+const int piezo_pin = 6;
+
+// ??????????, ?????????? ? ??????????
+int ldr_val = 0;        // ????????? ?????????? ???????? ?? ???????
+int piezo_freg = 0;
+
+// calibration vars
+int cmin = 1023;
+int cmax = 0;
+
+void calibrate () {
+
+    int tmp = 0;
+
+    for (int i=0; i < 100; i++) {
+        tmp = analogRead(ldr_pin);
+        if (tmp > cmax) {
+            cmax = tmp;
+        }
+        if (tmp < cmin) {
+            cmin = tmp;
+        }
+        delay(10);
+    }
+
+}
+
+void setup() {
+#if DEBUG
+    Serial.begin(9600);
+    Serial.print("setup");
+#endif
+
+    pinMode(ldr_pin, INPUT);
+    pinMode(piezo_pin, OUTPUT);
+
+    calibrate();
+}
+
+void loop() {
+
+    ldr_val = analogRead(ldr_pin);
+
+    if (ldr_val < cmin-5) {
+        piezo_freg     = map(ldr_val, 0, cmax, 1100, 6830);
+
+#if DEBUG
+        Serial.print("Num: ");
+        Serial.print(led_brightness);
+        Serial.print("\n");
+        delay(30);
+#endif
+
+        // and now the music!
+        digitalWrite(piezo_pin, HIGH);
+        delayMicroseconds(piezo_freg / 2);
+
+       // DOWN
+       digitalWrite(piezo_pin, LOW);
+       delayMicroseconds(piezo_freg / 2);
+    }
+
+}
